460fbffe9be716f5f98cf286e7c3ae887d8d38a9
gelton cruz <gelton@midiaplural.com.br>
2016-07-20 21:09:50 -0300

    Follow Camera

---
+++ 01/PrimeiraAula/Assets/scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject follow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Chamada apos processamento físico.
+	void LateUpdate () {
+		Vector3 followPosition = follow.transform.position;
+		Vector3 cameraPosition = new Vector3 (
+			followPosition.x, followPosition.y, transform.position.z
+		);
+		transform.position = cameraPosition;
+	}
+}
